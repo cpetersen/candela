037c0243d71c39025bb2eb838429d753ca656b10
Chris Petersen <chris@petersen.io>
2025-04-28 12:54:26 -0700

    Small spec refactor

--- spec/candela/tensor_spec.rb
+++ spec/candela/tensor_spec.rb
@@ -1,23 +1,22 @@
 # frozen_string_literal: true
 
-RSpec.describe Candela do
+RSpec.describe Candela::Tensor do
   it "has a version number" do
     expect(Candela::VERSION).not_to be nil
   end
 
   describe Candela::Tensor do
+    let(:tensor) { Candela::Tensor.new([1.0, 2.0, 3.0, 4.0], [2, 2]) }
+
     it "can create a tensor" do
-      tensor = Candela::Tensor.new([1.0, 2.0, 3.0, 4.0], [2, 2])
       expect(tensor).to be_a(Candela::Tensor)
     end
 
     it "can get the shape of a tensor" do
-      tensor = Candela::Tensor.new([1.0, 2.0, 3.0, 4.0], [2, 2])
       expect(tensor.shape).to eq([2, 2])
     end
 
     it "can convert a tensor to a vector" do
-      tensor = Candela::Tensor.new([1.0, 2.0, 3.0, 4.0], [2, 2])
       expect(tensor.to_vec).to eq([1.0, 2.0, 3.0, 4.0])
     end
   end
